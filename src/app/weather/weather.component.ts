8d13115c242b55616ca7ccb3b0ea5b455ecc6305
rincyvr <rincycse@gmail.com>
2020-10-19 18:06:23 +0530

    3 files modified, 3 deleted

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { WeatherApiService } from '../weather-api.service';
+import { Component, OnInit } from "@angular/core";
+import { WeatherApiService } from "../weather-api.service";
 
 @Component({
-  selector: 'app-weather',
-  templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.scss']
+  selector: "app-weather",
+  templateUrl: "./weather.component.html",
+  styleUrls: ["./weather.component.scss"]
 })
 export class WeatherComponent implements OnInit {
+  weathers: any = [];
 
-  weathers : any = [];
-
-  constructor(private service: WeatherApiService) { }
+  constructor(private service: WeatherApiService) {}
 
   ngOnInit(): void {
     // for(let i = 0; i<= 8; i++){
@@ -20,83 +19,83 @@ export class WeatherComponent implements OnInit {
   }
 
   getWeather(i) {
-    console.log("the valueis ", (<HTMLInputElement>document.getElementById('location-'+i)).value)
-    let city = (<HTMLInputElement>document.getElementById('location-'+i)).value;
-    this.service.getWeather(city).subscribe(res => {
-      this.weathers[i] = {location : city,data : res,editFlag : true,msg : null}
-      // this.weathers[i].location = city;
-      // this.weathers[i].data = res;
-      // this.weathers[i].editFlag = true;
-      // this.weathers[i].msg = null;
-    },error=>{
-      this.weathers[i].msg = error.error.message;
-      this.weathers[i].editFlag = false;
-    })
+    let city = (<HTMLInputElement>document.getElementById("location-" + i))
+      .value;
+    this.service.getWeather(city).subscribe(
+      res => {
+        this.weathers[i] = {
+          location: city,
+          data: res,
+          editFlag: true,
+          msg: null
+        };
+      },
+      error => {
+        this.weathers[i].msg = error.error.message;
+        this.weathers[i].editFlag = false;
+      }
+    );
   }
   changeCity(i) {
     this.weathers[i].editFlag = false;
     this.weathers[i].msg = null;
   }
- initialize(){
-   this.weathers = [{
-    location : "",
-    editFlag : false,
-    msg : "",
-    data : {
-    }
-  },{
-    location : "",
-    editFlag : false,
-    msg : "",
-    data : {
-    }
-  },{
-    location : "",
-    editFlag : false,
-    msg : "",
-    data : {
-    }
-  },{
-    location : "",
-    editFlag : false,
-    msg : "",
-    data : {
-    }
+  initialize() {
+    this.weathers = [
+      {
+        location: "",
+        editFlag: false,
+        msg: "",
+        data: {}
+      },
+      {
+        location: "",
+        editFlag: false,
+        msg: "",
+        data: {}
+      },
+      {
+        location: "",
+        editFlag: false,
+        msg: "",
+        data: {}
+      },
+      {
+        location: "",
+        editFlag: false,
+        msg: "",
+        data: {}
+      },
+      {
+        location: "",
+        editFlag: false,
+        msg: "",
+        data: {}
+      },
+      {
+        location: "",
+        editFlag: false,
+        msg: "",
+        data: {}
+      },
+      {
+        location: "",
+        editFlag: false,
+        msg: "",
+        data: {}
+      },
+      {
+        location: "",
+        editFlag: false,
+        msg: "",
+        data: {}
+      },
+      {
+        location: "",
+        editFlag: false,
+        msg: "",
+        data: {}
+      }
+    ];
   }
-  ,{
-    location : "",
-    editFlag : false,
-    msg : "",
-    data : {
-    }
-  }
-  ,{
-    location : "",
-    editFlag : false,
-    msg : "",
-    data : {
-    }
-  }
-  ,{
-    location : "",
-    editFlag : false,
-    msg : "",
-    data : {
-    }
-  }
-  ,{
-    location : "",
-    editFlag : false,
-    msg : "",
-    data : {
-    }
-  }
-  ,{
-    location : "",
-    editFlag : false,
-    msg : "",
-    data : {
-    }
-  }]
- }
 }
